80f86ab35586403daee1b3c61272029b5505c5eb
vincentnguyen07 <vince.nguyen0709@gmail.com>
2019-03-28 23:49:34 -0700

    Added Books Detail view

--- src/ComicBookGallery/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -10,13 +10,8 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Thursday)
-            {
-                return Redirect("/");
-            }
-            
-            return Content( "Hello from the comic book controller!");
-            
+            return View();
+ 
         }
     }
 }
